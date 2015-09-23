d492f41ae56243a0ba43c671785ce6d45fd5e147
w0285677 <0285677@nscc.ca>
2015-09-23 09:57:37 -0300

    Completed averages calculator

--- Averages Calculator.py
+++ Averages Calculator.py
@@ -1,1 +1,16 @@
-__author__ = 'martindavidson'
+__author__ = '0285677'
+
+#1. Prompt the user to enter three numbers.
+print("We're gonna need ya to enter three numbers...")
+numberOne = float(input("First number: "))
+numberTwo = float(input("Second number: "))
+numberThree = float(input("Third number: "))
+
+#2. Display a message confirming the numbers the user entered.
+print("You did it! They're all numbers!")
+
+#3. Calculate the average of the three numbers.
+average = (numberOne + numberTwo + numberThree) / 3.0
+
+#4. Display the average, rounded to two decimal places.
+print("The average of your numbers is: " + str(round(average, 2)))
